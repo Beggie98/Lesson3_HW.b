8c8d5b906df40d42c17e1157a37fa939841ab9d4
Begi <123@gmail.com>
2023-01-03 13:20:39 +0500

    save

---
+++ main.js
@@ -0,0 +1,14 @@
+let num = +prompt('Введите количество')
+let sym = prompt('Введите симбол')
+let end = prompt('Введите конечный символ')
+
+let i = 1;
+let sym1 = '';
+
+do{
+    console.log(sym1 + end);
+    sym1 = sym1 + sym
+    i++
+}while (i <= num) {
+    
+}
